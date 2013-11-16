c576ae4ed78cf4c05425b29b598962f454f796a2
dengjiebin <dengjiebin@ujuhui.com>
2013-11-16 22:59:13 +0800

    fix bug: parse the parameters error. make the iframe align center

--- youku.rb
+++ youku.rb
@@ -1,27 +1,26 @@
 class Youku < Liquid::Tag
-Syntax = /^\s*([^\s]+)(\s+(\d+)\s+(\d+)\s*)?/
 
 def initialize(tagName, markup, tokens)
   super
   
-  if markup =~ Syntax then
-    @id = $1
-    
-    if $2.nil? then
+  @params = markup.split(" ")
+  if @params.count >= 1
+    @id = @params[0]
+    if @params.count >= 3
+      @width = @params[1]
+      @height = @params[2]
+    else
       @width = 560
       @height = 420
-    else
-      @width = $2.to_i
-      @height = $3.to_i
     end
   else
-    raise "No Youku ID provided in the \"youku\" tag"
+    raise "No Youku ID provided in the \"youku\" tag"    
   end
 end
 
 def render(context)
 # "<iframe height=498 width=510 src="http://player.youku.com/embed/XNTEzNzcwNDI0" frameborder=0 allowfullscreen></iframe>"
-"<iframe height=\"#{@height}\" width=\"#{@width}\" src=\"http://player.youku.com/embed/#{@id}?color=white&theme=light\"></iframe>"
+"<iframe style=\"margin:0 auto; display: block\" height=\"#{@height}\" width=\"#{@width}\" src=\"http://player.youku.com/embed/#{@id}?color=white&theme=light\"></iframe>"
 end
 
 Liquid::Template.register_tag "youku", self
--- youtube.rb
+++ youtube.rb
@@ -3,21 +3,21 @@ Syntax = /^\s*([^\s]+)(\s+(\d+)\s+(\d+)\s*)?/
 
 def initialize(tagName, markup, tokens)
   super
-  
-  if markup =~ Syntax then
-    @id = $1
-    
-    if $2.nil? then
+
+  @params = markup.split(" ")
+  if @params.count >= 1
+    @id = @params[0]
+    if @params.count >= 3
+      @width = @params[1]
+      @height = @params[2]
+    else
       @width = 560
       @height = 420
-    else
-      @width = $2.to_i
-      @height = $3.to_i
     end
   else
     raise "No YouTube ID provided in the \"youtube\" tag"
   end
-end
+end    
 
 def render(context)
 # "<iframe width=\"#{@width}\" height=\"#{@height}\" src=\"http://www.youtube.com/embed/#{@id}\" frameborder=\"0\"allowfullscreen></iframe>"
